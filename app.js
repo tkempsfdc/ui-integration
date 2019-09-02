f0db3947ded654248bd61e85c786c12313a0f8b8
Fakhruddin Faizi <ffaizi@ffaizi-ltm3.internal.salesforce.com>
2019-09-02 10:53:34 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var nforce = require('nforce');
+var express = require('express');
+var port = process.env.PORT || 3000;
+
+var org = nforce.createConnection({
+  clientId: process.env.CONSUMER_KEY,
+  clientSecret: process.env.CONSUMER_SECRET,
+  redirectUri: 'https://my-ui-integration.herokuapp.com/oauth/_callback',
+  apiVersion: 'v46.0',  // optional, defaults to current salesforce API version
+  environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+  mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
+});
+
+var app = express();
+
+// Require Routes js
+var routesHome = require('./routes/home');
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+
+app.use('/home', routesHome);
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+  res.redirect(org.getAuthUri());
+});
+
+app.get('/oauth/_callback', function(req, res) {
+  org.authenticate({code: req.query.code}, function(err, resp){
+    if(!err) {
+      console.log('Access Token: ' + resp.access_token);
+      app.locals.oauthtoken = resp.access_token;
+      app.locals.lightningEndPointURI = process.env.LIGHTNING_URL;
+      console.log('Access Token #2: ' + app.locals.oauthtoken);
+      console.log('LightningEndpoint URI: ' +  app.locals.lightningEndPointURI);
+      res.redirect('/home');
+    } else {
+      console.log('Error: ' + err.message);
+    }
+  });
+});
+
+// Served Localhost
+console.log('Served: http://localhost:' + port);
+app.listen(port);
