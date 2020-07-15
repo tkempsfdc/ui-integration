7afdb91532ddd295dd4bf60b09d755ee4b7a0e96
Tina Kemp <tkempsfdc@users.noreply.github.com>
2020-07-15 10:58:27 -0500

    revert to app.locals

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/oauth/_callback', function(req, res) {
       console.log('Access Token: ' + resp.access_token);
       app.locals.oauthtoken = resp.access_token;
       app.locals.lightningEndPointURI = process.env.LIGHTNING_URL;
-      var myVar = app.locals.lightningEndPointURI + '/lightning/lightning.out.js';
+      app.locals.completelightningEndPointURI = app.locals.lightningEndPointURI + '/lightning/lightning.out.js';
       console.log('Access Token #2: ' + app.locals.oauthtoken);
       console.log('LightningEndpoint URI: ' +  app.locals.lightningEndPointURI);
       res.redirect('/home');
